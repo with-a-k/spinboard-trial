90a7524ff496b13fe424b8802d9046bccbd8dd95
Erik Butcher <with_a_k@mac.com>
2015-11-18 19:41:36 -0700

    Fix issue in which a freshly-signed-up user couldn't create links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
-      cookies.signed[:session] = { user: user.id, expires: Time.now + 3600 }
+      cookies.signed[:session] = { value: user.id, expires: Time.now + 3600 }
       redirect_to links_path
     else
       flash[:whoops] = "Couldn't sign you up. Someone else is using that email address, or your password didn't match its confirmation."
